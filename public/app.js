d42a55884cdbece071bf621c253ae1873eb0e5e5
Mary Frances <mfskibiel@gmail.com>
2020-01-07 17:40:17 -0500

    tried to fix some errors

--- public/app.js
+++ public/app.js
@@ -1,60 +1,57 @@
-$(document).ready(function() {
-  console.log("good to go");
-  $.getJSON("/articles", function(data) {
-    for (var i = 0; i < data.length; i++) {
-      //display articles on page
-      $("#results").append(
-        '<li data-id="' +
-          data[i]._id +
-          '"><a class = "links" href= "' +
-          data[i].link +
-          '</a><div class="caption"><h3>' +
-          data[i].title +
-          "</h3></div></li>"
-      );
-    }
+$.ajax({
+  type: "GET",
+  url: "/articles"
+}).then(function(response) {
+  console.log(response);
+
+  const articleResults = $("#results");
+
+  for (i = 0; i < response.length; i++) {
+    const article = response[i];
+
+    const saveButton = $("<button>")
+      .addClass("saveButton")
+      .text("Save")
+      .attr("id", article._id);
+
+    const title = $("<div>")
+      .addClass("title")
+      .text(article.title)
+      .append(saveButton);
+
+    const link = $("<a>")
+      .addClass("link")
+      .text(article.link)
+      .attr("href", article.link)
+      .attr("target", "_blank");
+
+    const listItem = $("<li>")
+      .addClass("article")
+      .append(title, link);
+
+    articleResults.append(listItem);
+  }
+});
+
+hideContainer();
+showScrapeResults();
+
+$(document).on("click", ".saveButton", function() {
+  var articleId = $(this).attr("id");
+  console.log("Article ID: " + articleId);
+
+  $.ajax({
+    type: "PUT",
+    url: "/save-article/" + articleId
+  }).then(function(response) {
+    console.log(JSON.stringify(response));
   });
 });
 
-$(document).on("click", ".btn", function() {
-  var buttonid = $(this).attr("id");
-  console.log(buttonid);
-  if (buttonid === "add-note") {
-    var thisId = $(this).attr("value");
+// const hideContainer = function() {
+//   $("#container").hide();
+// };
 
-    $.ajax({
-      method: "GET",
-      url: "/articles/" + thisId
-    }).done(function(data) {
-      console.log(data);
-      $("#modalTitle").text(data.title);
-      $("#save-note").attr("data-id", thisId);
-      $("#note-text").attr("data-id", thisId);
-
-      if (data.note) {
-        $("#titleinput").val(data.note.title);
-        $("#bodyinput").val(data.note.body);
-        $("#note-text")
-          .attr("data-id", thisId)
-          .text("note added");
-      }
-    });
-  } else if (buttonid === "save-note") {
-    var saveId = $(this).attr("data-id");
-    console.log(saveId);
-
-    $.ajax({
-      method: "POST",
-      url: "/articles/" + saveId,
-      data: {
-        title: $("#titleinput").val(),
-        body: $("#bodyinput").val(),
-        timestamp: Date.now()
-      }
-    }).done(function(data) {
-      $("#note-text")
-        .attr("data-id", this.Id)
-        .text("note saved");
-    });
-  }
-});
+// const showScrapeResults = function() {
+//   $("#scrapeResults").show(600);
+// };
--- server.js
+++ server.js
@@ -2,21 +2,97 @@ const express = require("express");
 const mongoose = require("mongoose");
 const logger = require("morgan");
 
+var axios = require("axios");
+var cheerio = require("cheerio");
+
 var db = require("./models");
 
-var PORT = process.env.PORT || 3030;
+var PORT = 3000;
 
-const app = express();
+var app = express();
 
 app.use(logger("dev"));
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require("./routes.js");
-
 mongoose.connect("mongodb://localhost/scienceWebScraping", {
   useNewUrlParser: true
+});
+
+//ROUTES
+
+app.get("/scrape", function(req, res) {
+  axios.get("https://www.insidescience.org/earth").then(function(response) {
+    var $ = cheerio.load(response.data);
+
+    $(".field-title h2 a").each(function(i, element) {
+      var result = {};
+      console.log(i);
+      result.title = $(this).text();
+      result.link = $(this).attr("href");
+      console.log(result.title);
+
+      // Create a new Article using the `result` object built from scraping
+      db.Article.create(result)
+        .then(function(dbArticle) {
+          console.log(dbArticle);
+        })
+        .catch(function(err) {
+          res.json(err);
+        });
+
+      res.send("it worked");
+    });
+  });
+});
+
+app.get("/clear", function(req, res) {
+  db.Article.remove({})
+    .then(function(dbArticle) {
+      res.redirect("/");
+    })
+    .catch(function(err) {
+      res.json(err);
+    });
+});
+
+app.get("/articles", function(req, res) {
+  db.Article.find({})
+    .then(function(dbArticle) {
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      res.json(err);
+    });
+});
+
+app.get("/articles/:id", function(req, res) {
+  db.Article.findOne({ _id: req.params.id })
+    .populate("note")
+    .then(function(dbArticle) {
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      res.json(err);
+    });
+});
+
+app.post("/articles/:id", function(req, res) {
+  db.Note.create(req.body)
+    .then(function(dbNote) {
+      return db.Article.findOneAndUpdate(
+        { _id: req.params.id },
+        { note: dbNote._id },
+        { new: true }
+      );
+    })
+    .then(function(dbArticle) {
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      res.json(err);
+    });
 });
 
 app.listen(PORT, function() {
